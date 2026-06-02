489e781577f512f830b3f959590cabe26fc640cc
Wilson Caleb <wilsonperepelecia@gmail.com>
2026-06-02 16:18:05 -0300

    Criada internacionalização de analises do arquivo

--- src/main/java/report/ReportMain.java
+++ src/main/java/report/ReportMain.java
@@ -1,23 +1,60 @@
 package report;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+
 public class ReportMain {
 
-    public static void main(String[] args) {
+    void main(String[] args) {
+
+        String language = "PT";
+
+        showAnalitcs(language);
+    }
+
+    private void showAnalitcs(String language) {
         TransactionReport transactionReport = new TransactionReport();
+
+        Locale locale = getByLocaleByLanguage(language);
 
         TransactionReport.ReportAnalitics reportAnalitcs = transactionReport.getReportAnalitcs("data/log.csv");
 
-        System.out.println("""
-                Total de linhas: %d
-                Total de fraudes: %d
-                Valor total transacionado: %f
-                """.formatted(reportAnalitcs.totalLines(), reportAnalitcs.totalFrauds(), reportAnalitcs.totalAmount()));
+        NumberFormat numberFormat = NumberFormat.getCurrencyInstance(locale);
 
-//        Stream<String> linesStream = transactionReport.getLinesStream("data/log.csv");
-//
-//        System.out.println(linesStream.count());
+        ResourceBundle bundle = getResourceBundleByLocale(getBundlePathByLanguage(language), locale);
+
+        System.out.printf("""
+                        %s: %d
+                        %s: %d
+                        %s %s
+                        %n""", bundle.getString("transaction.report.totallinhas"), reportAnalitcs.totalLines(),
+                bundle.getString("transaction.report.totalfraudes"), reportAnalitcs.totalFrauds(),
+                bundle.getString("transaction.report.valortotal"), numberFormat.format(reportAnalitcs.totalAmount()));
     }
 
+    private String getBundlePathByLanguage(String language) {
+        if ("US".equals(language)) {
+            return "report_en";
+        }
+        if ("PT".equals(language)) {
+            return "report_pt_BR";
+        }
+        throw new RuntimeException("Linguagem selecionada não disponível ou inválida");
+    }
 
+    private ResourceBundle getResourceBundleByLocale(String bundlePath, Locale locale) {
+        return ResourceBundle.getBundle(bundlePath, locale);
+    }
 
+    private Locale getByLocaleByLanguage(String language) {
+        if ("US".equals(language)) {
+            return Locale.US;
+        }
+        if ("PT".equals(language)) {
+            return Locale.of("pt", "BR");
+        }
+        throw new RuntimeException("Arquivo de propriedades de internacionalização não encontrado");
+    }
 }
